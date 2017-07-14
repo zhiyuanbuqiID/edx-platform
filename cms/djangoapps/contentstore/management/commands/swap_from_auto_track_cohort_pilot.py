9a0c292e5c85fdaf339113118176a6003ff1d7bc
Jeff LaJoie <jlajoie@edx.org>
2017-07-14 08:33:10 -0400

    initial changes for getting xblocks

--- cms/djangoapps/contentstore/management/commands/swap_from_auto_track_cohort_pilot.py
+++ cms/djangoapps/contentstore/management/commands/swap_from_auto_track_cohort_pilot.py
@@ -3,13 +3,12 @@ from opaque_keys import InvalidKeyError
 from opaque_keys.edx.keys import CourseKey
 from opaque_keys.edx.locations import SlashSeparatedCourseKey
 
-from xmodule.contentstore.django import contentstore
 from xmodule.modulestore.django import modulestore
 
 
 class Command(BaseCommand):
     """
-
+    Be sure to run with --settings=devstack
     """
     help = ''
 
@@ -21,6 +20,9 @@ class Command(BaseCommand):
         """
         module_store = modulestore()
 
+        # TODO ensure old feature for course is disabled
+        # TODO ensure verified track has been created
+
         try:
             course_key = CourseKey.from_string(options['course_id'])
         except InvalidKeyError:
@@ -28,7 +30,13 @@ class Command(BaseCommand):
                 course_key = SlashSeparatedCourseKey.from_string(options['course_id'])
             except InvalidKeyError:
                 raise CommandError("Invalid course_key: '%s'." % options['course_id'])
-
-        if not module_store.get_course(course_key):
+        items = module_store.get_items(course_key)
+        if not items:
             raise CommandError("Course with %s key not found." % options['course_id'])
+        for item in items:
+            # TODO check that group_access is set to "enrollment cohort content groups" and change
+            # TODO their variable to enrollment track user partition
+            if item.group_access != {}:
+                for key, value in item.group_access.iteritems():
+                    print value
 
