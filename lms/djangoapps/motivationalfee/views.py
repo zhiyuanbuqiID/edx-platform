edaa89dcae84b48966310fa32f4ea86c1ef12028
Brittney Exline <bexline@edx.org>
2017-09-28 00:23:50 -0400

    Adding in a little bit of view logic

--- lms/djangoapps/motivationalfee/views.py
+++ lms/djangoapps/motivationalfee/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 
-# Create your views here.
+from django.conf import settings
+from django.views.generic import View
+from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
+import requests
+from six.moves.urllib.parse import urlencode
+
+
+class CourseEnrollmentView(View):
+    """
+
+    """
+
+    def get(self, request):
+        """
+        The track selection page can be modified to redirect here, and this can render a template with the fb login logic.
+        """
+        pass
+
+    def post(self, request):
+        """
+
+        """
+
+        course_run_id = request.POST.get('course_run_id')
+        fb_access_token = request.POST.get('fb_access_token')
+        fb_user_id = request.POST.get('fb_user_id')
+
+        fb_app_id = configuration_helpers.get_value('HACKATHON_FB_APP_ID', settings.HACKATHON_FB_APP_ID)
+        fb_app_secret = configuration_helpers.get_value('HACKATHON_FB_APP_SECRET', settings.HACKATHON_FB_APP_SECRET)
+
+        response = requests.get(
+            '{fb_url}?{params}'.format(
+                fb_url='http://graph.facebook.com/oauth/access_token',
+                params=urlencode((
+                    ('grant_type', 'fb_exchange_token'),
+                    ('client_id', fb_app_id),
+                    ('client_secret', fb_app_secret),
+                    ('fb_exchange_token', fb_access_token)
+                ))
+            )
+
+        # store the token from the response in the model and redirect to dashboard
+
+        )
