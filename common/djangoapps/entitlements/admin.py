d13e3bc7ddc79ae3e4480133b2a23baf1c0e5679
McKenzie Welter <mwelter@edx.org>
2017-12-06 09:04:20 -0500

    entitlement admin page form

--- common/djangoapps/entitlements/admin.py
+++ common/djangoapps/entitlements/admin.py
@@ -3,6 +3,20 @@ from django.contrib import admin
 from .models import CourseEntitlement
 
 
+class EntitlementForm(forms.ModelForm):
+    """
+    Django admin form for Entitlement model
+    """
+    def __init__(self, *args, **kwargs):
+        super(EntitlementForm, self).__init__(*args, **kwargs)
+        self.fields['expired_at'] = forms.SplitDateTimeField(
+            required=False, empty_value=None
+        )
+
+    class Meta(object):
+        model = MicrositeOrganizationMapping
+        fields = '__all__'
+
 @admin.register(CourseEntitlement)
 class EntitlementAdmin(admin.ModelAdmin):
     list_display = ('user',
